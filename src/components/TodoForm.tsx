10104548faf18eeea1ee7908b9b1bce11997a79b
monsterberget <manspbk@gmail.com>
2025-05-08 20:54:23 +0200

    added tofoform

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,7 +1,37 @@
 import { ChangeEvent, FormEvent } from "react";
 import { defaultvalue, Todo } from "../models/Todo";
+import { useState } from "react";
 
 
-type AddTodoProps = {
+type TodoFormProps = {
     addTodo: (Todo: Todo) => void;
 };
+
+export const TodoForm = ({addTodo}: TodoFormProps) => {
+    const [inputValue, setInputValue] = useState<string>("");
+    const [todo, setTodo] = useState<Todo>(defaultvalue);
+    
+    const handlesubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        addTodo({ ...todo, title: inputValue });
+        setInputValue("");
+    };
+    return (
+        <form className='todo-form' onSubmit={handlesubmit}>
+            <input
+             value={inputValue}
+             className="flex-grow px-3 py-3 h-14 border pr-2 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+             type="text" 
+             placeholder="New Todo"
+             onChange={(e) => setInputValue(e.target.value)} 
+                
+            />
+            <button
+             className='bg-amber-500 border-none rounded-full bg-range-600 w-32 h-14 text-white text-lg font-medium cursor-pointer' type="submit"
+             >
+            Add Todo
+            </button>
+            </form>
+    );
+}
+export default TodoForm;
